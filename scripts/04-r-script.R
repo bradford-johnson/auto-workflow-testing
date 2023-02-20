be30bcbe1d24ada235d18f12b8d991bc73378424
Ford Johnson <111791965+bradfordjohnson@users.noreply.github.com>
2023-02-19 22:15:49 -0500

    Update 04-r-script.R

--- scripts/04-r-script.R
+++ scripts/04-r-script.R
@@ -5,12 +5,21 @@ library(htmltools)
 
 showtext_auto()
 
+font_add(family = "fb",
+         regular = "fonts/Font Awesome 6 Brands-Regular-400.otf")
+
+caption = paste0("<span style='font-family:fb;'>&#xf09b;</span>",
+                 "<span style='font-family:sans;color:white;'>.</span>",
+                 "<span style='font-family:sans;'>bradfordjohnson</span>")
+
 df <- gapminder
 
 df |>
   filter(country == "United States") |>
   ggplot(aes(x = year, y = lifeExp)) +
   geom_line() +
-  labs(title = "This is a test plot")
+  labs(title = "This is a test plot",
+      caption = caption) +
+  theme(plot.caption = ggtext::element_textbox_simple(color="black", size = 10))
 
 ggsave("plot.png", width = 5, height = 5)
