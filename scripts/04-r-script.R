020264160bfee4474836f2609e7528ea1a4c7052
Ford Johnson <111791965+bradfordjohnson@users.noreply.github.com>
2023-02-19 23:03:33 -0500

    Update 04-r-script.R

--- scripts/04-r-script.R
+++ scripts/04-r-script.R
@@ -23,6 +23,19 @@ df |>
   geom_line() +
   labs(title = "This is a test plot",
       caption = caption) +
-  theme(plot.caption = ggtext::element_textbox_simple(color="black", size = 10))
+   theme(
+      plot.title = element_text(family = font, face = "bold", hjust = 0.5, vjust = 0, size = 58),
+      
+      plot.caption = ggtext::element_textbox_simple(size = 26),
+      plot.margin = unit(c(10,20,10,7), "pt"),
+      axis.title.y = element_text(family = font, face = "bold", margin = margin(0,3,0,0, unit = "mm"), size = 34),
+      axis.text.y = element_text(family = font, color = "black", size = 26),
+      axis.text.x = element_text(family = font, color = "black", size = 26),
+      panel.grid.major = element_line(colour = "gray90"),
+      panel.grid.minor = element_blank(),
+      legend.position = "none",
+      text = element_text(family = font),
+      panel.background = element_blank(),
+      plot.background = element_rect(fill = "#F3F5F6", color = "#F3F5F6"))
 
-ggsave("plot.png", width = 5, height = 5)
+ggsave("plot.png", width = 9, height = 9)
